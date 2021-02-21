8caffbb9772aea951b9b6689dddf7b3172eff7f0
Дмитрий Фильков <dima.filkov01@mail.ru>
2021-02-21 16:26:14 +0300

    Button
    Сделал зажатие UI Button'a

---
+++ UITest/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveCube : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public GameObject Player;
+    public bool ifButtonPressed;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ifButtonPressed = true;
+        Debug.Log("d");
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        ifButtonPressed = false;
+        Debug.Log("u");
+    }
+    public void Update()
+    {
+        if (ifButtonPressed)
+        {
+            Player.transform.Translate(2 * Time.deltaTime, 0, 0);
+        }
+    }
+}
